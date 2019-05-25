b7e53663893b05930f0401259bf890aa3dcf3f6f
Tevvin <Tevvin7395@icloud.com>
2019-05-25 13:55:03 -0400

    Configured Knex to talk to the database

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+// Configure knex
+const knexConfig = {
+  client: 'sqlite3',
+  connection: {
+    filename: './data/lambda.sqlite3'
+  }
+};
+
+const db = knex(knexConfig);
 
 const server = express();
 
@@ -9,6 +20,6 @@ server.use(helmet());
 // endpoints here
 
 const port = 3300;
-server.listen(port, function() {
+server.listen(port, function () {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
